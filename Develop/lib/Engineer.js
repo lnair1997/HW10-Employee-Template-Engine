658749bf01556f1c32e6d840139110a7d58629a5
lathisha <lathisha.n@gmail.com>
2020-06-09 19:28:32 -0700

    extend manager

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -2,18 +2,18 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-    constructor(name, id, email, githubName){
+    constructor(name, id, email, githubName) {
         super(name, id, email);
         this.github = githubName;
     }
 
-    getGithub(){
+    getGithub() {
         return this.github;
     }
 
-    getRole(){
+    getRole() {
         return "Engineer";
     }
 }
 
-module.exports= Engineer;
+module.exports = Engineer;
